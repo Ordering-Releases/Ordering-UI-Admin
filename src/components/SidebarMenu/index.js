d95e9285af68c59ba3246e2971fe7727632b2ba0
carlosg-ordering <carlos.g@ordering.co>
2022-10-12 17:25:09 -0500

    [UPDATE] Hide Campaign v1.3.6

--- src/components/SidebarMenu/index.js
+++ src/components/SidebarMenu/index.js
@@ -261,13 +261,13 @@ export const SidebarMenu = (props) => {
       title: t('PROMOTIONS_ENTERPRISE', 'Promotions enterprise'),
       pageName: 'enterprise_promotions',
       url: '/marketing/promotions-enterprise'
-    },
-    {
-      id: 2,
-      title: t('CAMPAIGN', 'Campaign'),
-      pageName: 'campaign',
-      url: '/marketing/campaign'
     }
+    // {
+    //   id: 2,
+    //   title: t('CAMPAIGN', 'Campaign'),
+    //   pageName: 'campaign',
+    //   url: '/marketing/campaign'
+    // }
   ]
 
   const downloadsSubMenus = [
