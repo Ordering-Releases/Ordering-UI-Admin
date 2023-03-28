27b8dd4c860324058409bce17392291e38a43fba
carlosg-ordering <carlos.g@ordering.co>
2023-03-28 12:23:00 -0600

    build

--- _modules/components/Settings/SettingsList/index.js
+++ _modules/components/Settings/SettingsList/index.js
@@ -43,6 +43,7 @@ var SettingsListUI = function SettingsListUI(props) {
     handleInputChange = props.handleInputChange,
     handleClickUpdate = props.handleClickUpdate,
     saveConfig = props.saveConfig;
+  var settingsNotAllowed = ['guest_checkout_enabled', 'allowed_order_types_guest_checkout', 'platform_store_giftcard_enabled'];
   var _useLanguage = (0, _orderingComponentsAdminExternal.useLanguage)(),
     _useLanguage2 = _slicedToArray(_useLanguage, 2),
     t = _useLanguage2[1];
@@ -155,7 +156,7 @@ var SettingsListUI = function SettingsListUI(props) {
     height: 44
   })), !settingsState.error && !settingsState.loading && configs && !settingsState.API && /*#__PURE__*/_react.default.createElement(_styles2.GeneralContainer, null, /*#__PURE__*/_react.default.createElement(_styles2.FormContainer, null, configs.length > 0 && configs.map(function (config, i) {
     var _config$options, _config$options2, _config$options3, _config$options4;
-    return /*#__PURE__*/_react.default.createElement("div", {
+    return !settingsNotAllowed.includes(config.key) && /*#__PURE__*/_react.default.createElement("div", {
       key: i
     }, config.type === 1 && /*#__PURE__*/_react.default.createElement(_styles2.FormGroupText, {
       className: "form-group"
