9ca45563f77156bdaa741720acc9f483772c3bf1
carlosg-ordering <carlos.g@ordering.co>
2023-09-18 14:24:29 -0600

    [RELEASE] v1.38.0

--- index-template.js
+++ index-template.js
@@ -298,7 +298,7 @@ if (!(window?.location?.hostname === 'localhost')) {
     integrations: [
       new Integrations.BrowserTracing()
     ],
-    release: process.env.npm_package_version ? 'ordering-ui-admin-release@' + process.env.npm_package_version : 'ordering-ui-admin-release@' + '1.1.0',
+    release: process.env.npm_package_version ? 'ordering-ui-admin-release@' + process.env.npm_package_version : 'ordering-ui-admin-release@' + '1.1.1',
     // Release health
     autoSessionTracking: true,
     ignoreErrors: [
